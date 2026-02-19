4d1e98d66d600fbb23cc8d488e3660bda800f456
Aryan <chestnutario389@gmail.com>
2026-02-19 07:18:11 +0000

    Added routes for deletePost and createPost.

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,8 +1,10 @@
 const express = require("express");
-const { getAllPosts, getPostById } = require("../controllers/postController");
+const { getAllPosts, getPostById, deletePost, createPost } = require("../controllers/postController");
 const router = express.Router();
 
 router.get("/", getAllPosts);
 router.get("/:id", getPostById);
+router.delete("/:id", deletePost);
+router.post("/", createPost);
 
 module.exports = router;
